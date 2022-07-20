91f6f9c7cd09ebb981e7baf9466f598d87830c52
Shivam Kumar Chaurasia <helloshivam142@gmail.com>
2022-07-20 10:51:28 +0530

    Initial commit

---
+++ index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sum(TreeNode* root ,int curr,int &total)
+    {
+        if(root==NULL)
+            return;
+        curr=curr*2+root->val;
+        if(root->left==NULL && root->right==NULL)
+        {
+            total=total+curr;
+            return;
+        }
+        sum(root->left,curr,total);
+        sum(root->right,curr,total);
+        
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        int curr=0;
+        int total=0;
+        sum(root,curr,total);
+        return total;
+    }
+};
